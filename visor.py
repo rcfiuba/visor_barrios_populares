ce5daaf95ba3b7b898693c44c1e2bcc7a756e52f
Renzo Cammi <cammirenzo@gmail.com>
2023-03-18 19:43:21 -0300

    delete auth log in

--- visor.py
+++ visor.py
@@ -26,88 +26,61 @@ st.set_page_config(
     page_icon=':rocket:'
     )
 
-# authorized users 
-names = ['AGRIM_FIUBA']
-usernames = ['fiuba']
-passwords = ['fiub42023']
-
-# convert plain text to hashed passwords
-hashed_passwords = stauth.Hasher(passwords).generate()
-
-authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
-    'Barrios populares RENABAP', 'AGRIM_FIUBA', cookie_expiry_days=7)
-
-name, authentication_status, username = authenticator.login('Login','main')
-
-# if authentication is passed
-if authentication_status:
-
-    authenticator.logout('Logout', 'main')
-    st.write('Bienvenido *%s*' % (name))
-
-    st.title("Barrios Populares CABA")
+st.title("Barrios Populares CABA")
     
-    # get path
-    path = Path.cwd()
+# get path
+path = Path.cwd()
 
-    # open files
-    barrios_pop = gpd.read_file(str(path) + "/datos_GIS/shapefiles/poligono_barrios_populares_renabap.gpkg")
+# open files
+barrios_pop = gpd.read_file(str(path) + "/datos_GIS/shapefiles/poligono_barrios_populares_renabap.gpkg")
 
-    # set WGS84 as crs
-    barrios_pop_WGS84 = barrios_pop.to_crs(epsg=4326)
+# set WGS84 as crs
+barrios_pop_WGS84 = barrios_pop.to_crs(epsg=4326)
 
-    start_zoom = 12
+start_zoom = 12
 
-    # open map
-    m = folium.Map(location=[-34.62, -58.38], tiles='OpenStreetMap', zoom_start=start_zoom, control_scale=True) 
+# open map
+m = folium.Map(location=[-34.62, -58.38], tiles='OpenStreetMap', zoom_start=start_zoom, control_scale=True) 
 
-    # Add the full-screen control to the map
-    m.add_child(folium.plugins.Fullscreen())
+# Add the full-screen control to the map
+m.add_child(folium.plugins.Fullscreen())
 
-    barrios_pop_bordes = folium.GeoJson(
-        data=barrios_pop_WGS84, 
-        name='Barrios populares RENABAP', 
-        show=True  # This parameter will show the GeoJson layer by default
-    )   
+barrios_pop_bordes = folium.GeoJson(
+    data=barrios_pop_WGS84, 
+    name='Barrios populares RENABAP', 
+    show=True  # This parameter will show the GeoJson layer by default
+)   
 
-    fields = ['NOMBRE','Localidad','Departamen','SECCIÓN','MANZANA','Superficie','SIT_DOMINI','LEYES','Link_Ley','VIVI_AROX','Familias','CREACIÓN','TIPO','GAS','AGUA','CLOACAS','ELECTRICID']
-    folium.features.GeoJsonPopup(
+fields = ['NOMBRE','Localidad','Departamen','SECCIÓN','MANZANA','Superficie','SIT_DOMINI','LEYES','Link_Ley','VIVI_AROX','Familias','CREACIÓN','TIPO','GAS','AGUA','CLOACAS','ELECTRICID']
+folium.features.GeoJsonPopup(
         fields=fields,
         aliases=[s.upper() for s in fields],
         labels=True,
         localize=True,
         max_width=500,
-        ).add_to(barrios_pop_bordes)
+    ).add_to(barrios_pop_bordes)
 
-    # add base layers to map
-    folium.TileLayer('OpenStreetMap').add_to(m)
-    folium.TileLayer('Stamen Toner').add_to(m)
+# add base layers to map
+folium.TileLayer('OpenStreetMap').add_to(m)
+folium.TileLayer('Stamen Toner').add_to(m)
 
-    # add the GeoJson layer to the map
-    barrios_pop_bordes.add_to(m)
+# add the GeoJson layer to the map
+barrios_pop_bordes.add_to(m)
 
-    # add the tile layers to the layer control
-    folium.LayerControl(position='topright', 
-                        ).add_to(m)
+# add the tile layers to the layer control
+folium.LayerControl(position='topright', 
+                    ).add_to(m)
     
-    # Create a MousePosition plugin and add it to the map
-    mp = MousePosition(position='bottomright', separator=' | ')
-    mp.add_to(m)
+# Create a MousePosition plugin and add it to the map
+mp = MousePosition(position='bottomright', separator=' | ')
+mp.add_to(m)
 
-    # Define a function that resets the zoom level to the starting value
-    def reset_zoom():
-        m.fit_bounds(m.get_bounds())
+# Define a function that resets the zoom level to the starting value
+def reset_zoom():
+    m.fit_bounds(m.get_bounds())
 
-    # Create a Streamlit button that calls the reset_zoom() function when clicked
-    st.button("Reset Zoom", on_click=reset_zoom)
+# Create a Streamlit button that calls the reset_zoom() function when clicked
+st.button("Reset Zoom", on_click=reset_zoom)
 
-    # show static folium map on streamlit app
-    folium_static(m, width=1800, height=800)
-
-# if authentication is not passed
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
-
-# FOLLOW INST https://towardsdatascience.com/creating-interactive-maps-for-instagram-with-python-and-folium-68bc4691d075
+# show static folium map on streamlit app
+folium_static(m, width=1800, height=800)
